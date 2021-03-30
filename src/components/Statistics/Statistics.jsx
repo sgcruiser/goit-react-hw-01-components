599c888f8bc5e093a124f55d0c78adaf3841d2e7
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-03-31 01:12:26 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -29,8 +29,6 @@ const setBackgroundColorRandom = () =>
     .toString(16)
     .padStart(6, '0');
 
-// '#' + Math.floor(Math.random() * 16777215).toString(16);
-
 Statistics.defaultProps = {
   title: '',
 };
