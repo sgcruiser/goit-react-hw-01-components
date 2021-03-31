2c7af0124330c94d0783f1f800e44c8d6650c0cd
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-03-31 23:00:53 +0300

    task 3

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -7,29 +7,20 @@ import styles from './FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   return (
-    <ul className={styles.friend__list}>
+    <ul className={styles.list}>
       {friends.map(({ avatar, name, isOnline, id }) => (
-        <li key={id} class="friend__item">
-          <FriendListItem>
-            <span class="friend__status"></span>
-            <img class="friend__avatar" src={avatar} alt={name} width="48" />
-            <p class="friend__name">{name}</p>
-          </FriendListItem>
-        </li>
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id.toString()}
+        />
       ))}
     </ul>
   );
 };
 
 FriendList.propTypes = {
-  FriendListItem: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    }),
-  ),
+  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 export default FriendList;
-
-// {
-//   isOnline ? online : offline;
-// }
--- src/components/FriendList/FriendListItem/FriendsListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendsListItem.jsx
@@ -2,21 +2,29 @@ import PropTypes from 'prop-types';
 
 import defaultImage from './avatar.png';
 
-// import styles from './FriendListItem.module.css';
+import styles from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline, id }) => {
-  return;
+  const spanStatus = isOnline ? styles.active : styles.disabled;
+
+  return (
+    <li key={id} className={styles.item}>
+      <span className={spanStatus}>{isOnline}</span>
+      <img className={styles.avatar} src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
 };
 
 FriendListItem.defaultProps = {
-  url: defaultImage,
+  avatar: defaultImage,
 };
 
 FriendListItem.propTypes = {
-  url: PropTypes.string,
-  authorUrl: PropTypes.string.isRequired,
-  authorTag: PropTypes.string.isRequired,
-  quantity: PropTypes.number.isRequired,
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  id: PropTypes.string,
 };
 
 export default FriendListItem;
